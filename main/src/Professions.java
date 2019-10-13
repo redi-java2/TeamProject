dc758380c577d5cfd7b7fd13a85c8a660a5b90fc
Mohamed El Sherif <mohamed.elsherif@thoughtworks.com>
2019-10-13 22:00:26 +0200

    Added First Commit

---
+++ main/src/Professions.java
@@ -0,0 +1,50 @@
+public class Professions {
+
+  public static void main(String[] args) {
+    printDoctor();
+    printShopOwner();
+    printFarmerInfo();
+    printBakerInfo();
+    printZooKeeper();
+    printShoeMakerInfo();
+    printLibrarianInfo();
+    printTeacherInfo();
+    printEngineerInfo();
+  }
+
+  private static void printDoctor() {
+    System.out.println("I am a Doctor and I am");
+  }
+
+  private static void printEngineerInfo() {
+    System.out.println("I like ..");
+  }
+
+  private static void printTeacherInfo() {
+    System.out.println("I am a ..");
+  }
+
+  private static void printShoeMakerInfo() {
+    System.out.println("I love to make ..");
+  }
+
+  private static void printZooKeeper() {
+    System.out.println("I am a Zookeeper and ..");
+  }
+
+  private static void printBakerInfo() {
+    System.out.println("I own a ..");
+  }
+
+  private static void printFarmerInfo() {
+    System.out.println("I have 3 Cows and ..");
+  }
+
+  private static void printShopOwner() {
+    System.out.println("I have a Shop and ..");
+  }
+
+  private static void printLibrarianInfo() {
+    System.out.println("I love ..");
+  }
+}
