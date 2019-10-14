0fc6ca1b9f1b32142ff7291381db4a2bb989ed11
Tariq <https://github.com/TariqSaeed95/git-class.git>
2019-10-14 20:42:10 +0200

    Doc
    someSmallChangesInPrintingMethod

--- main/src/Professions.java
+++ main/src/Professions.java
@@ -1,60 +1,63 @@
 public class Professions {
 
-  public static void main(String[] args) {
-    printDoctor();
-    printShopOwner();
-    printFarmerInfo();
-    printBakerInfo();
-    printZooKeeper();
-    printShoeMakerInfo();
-    printLibrarianInfo();
-    printTeacherInfo();
-    printEngineerInfo();
-    printPoliceOfficerInfo();
-    printCarpenter();
-  }
+    public static void main(String[] args) {
+        printDoctor();
+        printShopOwner();
+        printFarmerInfo();
+        printBakerInfo();
+        printZooKeeper();
+        printShoeMakerInfo();
+        printLibrarianInfo();
+        printTeacherInfo();
+        printEngineerInfo();
+        printPoliceOfficerInfo();
+        printCarpenter();
+    }
 
-  private static void printDoctor() {
-    System.out.println("I am a Doctor and I am");
-  }
+    private static void printDoctor() {
+        System.out.println("I am a Doctor and I am a developer");
+    }
 
-  private static void printCarpenter() {
-    System.out.println("I am a Carpetner and I am");
-  }
+    private static void printCarpenter() {
+        System.out.println("I am a Carpetner and I am");
+    }
 
-  private static void printEngineerInfo() {
-    System.out.println("I like ...");
-  }
+    private static void printEngineerInfo() {
+        System.out.println("I like ...");
+    }
 
-  private static void printPoliceOfficerInfo() {
-    System.out.println("I am there to ..");
-  }
+    private static void printPoliceOfficerInfo() {
+        System.out.println("I am there to ..");
+    }
 
-  private static void printShoeMakerInfo() {
-    System.out.println("I love to make ..");
-  }
+    private static void printShoeMakerInfo() {
+        System.out.println("I love to make ..");
+    }
 
-  private static void printZooKeeper() {
-    System.out.println("I am a Zookeeper and ..");
-  }
+    private static void printZooKeeper() {
+        System.out.println("I am a Zookeeper and ..");
+    }
 
-  private static void printBakerInfo() {
-    System.out.println("I own a ...");
-  }
+    private static void printBakerInfo() {
+        System.out.println("I own a ...");
+    }
 
-  private static void printFarmerInfo() {
-    System.out.println("I have 3 Cows and ...");
-  }
+    private static void printFarmerInfo() {
+        System.out.println("I have 3 Cows and ...");
+    }
 
-  private static void printShopOwner() {
-    System.out.println("I have a Shop and ..");
-  }
+    private static void printShopOwner()
+    {
+        System.out.println("I have a Shop and ..");
+    }
 
-  private static void printLibrarianInfo() {
-    System.out.println("I love ...");
-  }
+    private static void printLibrarianInfo()
+    {
+        System.out.println("I love ...");
+    }
 
-  private static void printTeacherInfo() {
-    System.out.println("I am a ..");
-  }
+    private static void printTeacherInfo()
+    {
+        System.out.println("I am a ..");
+    }
 }
